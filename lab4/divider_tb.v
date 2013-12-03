3e91945540a0ba289406690122dfcc71173f5b96
Jonas Alexander Andersson <jonas@engr-e132-d03.engr.siu.edu>
2013-12-03 10:46:33 -0600

    started divider testbench file

--- lab4/divider_tb.v
+++ lab4/divider_tb.v
@@ -1,5 +1,23 @@
+`timescale 1ns/1ns
+
 module divider_tb;
-parameter n = 8;
-reg rem;
-initial begin 
-rem = 0;
+parameter n = 8, logn = 8;
+reg Clock, Resetn, s, LA, EB;
+reg [n-1:0] DataA, DataB;
+wire [n-1:0] R, Q;
+wire Done;
+
+divider divide(.Clock(Clock),
+               .Resetn(Resetn),
+               .s(s),
+               .LA(LA),
+               .EB(EB),
+               .DataA(DataA),
+               .DataB(DataB),
+               .R(R),
+               .Q(Q),
+               .Done(Done));
+
+
+
+endmodule
